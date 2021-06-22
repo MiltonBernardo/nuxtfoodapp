46995f36c9c5ec1d584f9b4712e5c88da96daaa4
Milton Bernardo <miltonbernardo11@gmail.com>
2021-06-22 20:13:02 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="container">
+    <app-header/>
+    <AppRestaurantInfo/>
+  </main>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppRestaurantInfo
+  
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,37 @@
+export const state = () => ({
+    fooddata:[]
+})
+
+/*export const getters = {
+    getterValue: state => {
+        return state.value
+    }
+}
+*/
+export const mutations = {
+    updateFoodData: (state, data) => {
+        state.fooddata = data
+    }
+}
+
+export const actions = {
+   async getFoodData({state, commit }) {
+     if(state.fooddata.length) return;
+     try{
+       await fetch('https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants',{
+         headers: {
+           'content-type': 'application/json',
+           'x-api-key': process.env.AwS_API_KEY
+         }
+       }).then(response => response.json())
+       .then(data=>{
+         //console.log(data);
+        commit('updateFoodData', data)
+       })
+      //
+     }catch(error){
+      console.log(error);
+     }
+       
+    }
+}
